1ba2ca228c4672438d2a31f816fb5dd12dc6f2fd
bit-user <bit-user@DESKTOP-H8SB14M>
2017-08-09 16:36:30 +0900

    RequestHandler - using 'static final' for 400 and 404 Root

--- src/http/RequestHandler.java
+++ src/http/RequestHandler.java
@@ -11,13 +11,17 @@ import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
 	private static final String DOCUMENT_ROOT = "./webapp";
+	private static final String ERROR_400_ROOT = "/error/400.html";
+	private static final String ERROR_404_ROOT = "/error/404.html";
 	private Socket socket;
 	
+	//생성자
 	public RequestHandler( Socket socket ) {
 		this.socket = socket;
 	}
 	
 	
+	//쓰레드 런 함수 오버라이딩 
 	@Override
 	public void run() {
 		try {
@@ -25,7 +29,6 @@ public class RequestHandler extends Thread {
 			InetSocketAddress inetSocketAddress = ( InetSocketAddress )socket.getRemoteSocketAddress();
 			consoleLog( "connected from " + inetSocketAddress.getAddress().getHostAddress() + ":" + inetSocketAddress.getPort() );
 
-			
 			// get IOStream
 			BufferedReader br = new BufferedReader( new InputStreamReader( socket.getInputStream(), "UTF-8") );
 			OutputStream os = socket.getOutputStream();
@@ -34,7 +37,7 @@ public class RequestHandler extends Thread {
 			while(true) {
 				String line = br.readLine();
 				
-				if ( line == null || "".equals( request) ) {
+				if ( line == null || "".equals( line) ) {
 					break;
 				}
 				
@@ -42,7 +45,6 @@ public class RequestHandler extends Thread {
 					request = line;
 					break;
 				}
-				
 			}
 			
 			consoleLog( "[Request] : "+request );
@@ -52,21 +54,13 @@ public class RequestHandler extends Thread {
 			if ("GET".equals( tokens[0] ) ) {
 				responseStaticResource( os, tokens[1], tokens[2] );
 			} else {
-				response400Error(os, tokens[2]);   // http/1.1 400 bad request \r\n
+				response400Error(os, tokens[2]);   
 				consoleLog("#Bad Request");
 			}
-			
-			// 예제 응답입니다.
-			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
-//			os.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
-//			os.write( "Content-Type:text/html; charset=utf-8\r\n".getBytes( "UTF-8" ) );
-//			os.write( "\r\n".getBytes() );
-//			os.write( "<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes( "UTF-8" ) );
 
 		} catch( Exception ex ) {
 			consoleLog( "error:" + ex );
 		} finally {
-			// clean-up
 			try{
 				if( socket != null && socket.isClosed() == false ) {
 					socket.close();
@@ -76,44 +70,46 @@ public class RequestHandler extends Thread {
 			}
 		}			
 	}
-
+	
+	
+	// http/1.1 200 OK \r\n    -Get방식 호출 
 	private void responseStaticResource( OutputStream os, String url, String protocol ) throws IOException {
 		if ("/".equals( url) ) {
 			url = "/index.html";
 		}
 		
 		File file = new File( DOCUMENT_ROOT + url );
-		
 		sendResponse(file, os, url, protocol, "200 OK");
 	}
 	
+	
 	// http/1.1 400 bad request \r\n
 	private void response400Error( OutputStream os, String protocol) throws IOException {
-		String url = "/error/400.html";
+		String url = ERROR_400_ROOT;
 		
 		File file = new File(DOCUMENT_ROOT + url);
-		
 		sendResponse(file, os, url, protocol, "400 BAD Request");
 	}
 	
+	
 	// HTTP/1.1 404 file not found \r\n
 	private void response404Error( OutputStream os, String protocol) throws IOException {
-		String url = "/error/404.html";
+		String url = ERROR_404_ROOT;
 		
 		File file = new File(DOCUMENT_ROOT + url);
-		
 		sendResponse(file, os, url, protocol, "404 File NotFound");
 	}
 	
-	
+		
+	//해당 파일 존재유무 확인 및 헤더/바디 데이터 전송 
 	private void sendResponse(File file, OutputStream os, String url, String protocol, String msg) throws IOException {
 		if( file.exists() == false ) {
 			//파일이 존재하지 않아..
 			consoleLog("#404 Error");
+			//여기...404가 문제 있으면 무한루프야...
 			if ( !"404 File NotFound".equals(msg) ) {
 				response404Error(os, protocol);
 			}
-			
 			return;
 		}
 		
@@ -129,8 +125,7 @@ public class RequestHandler extends Thread {
 	}
 	
 	
-	
-	
+	//로그 함수
 	public void consoleLog( String message ) {
 		System.out.println( "[RequestHandler#" + getId() + "] " + message );
 	}
